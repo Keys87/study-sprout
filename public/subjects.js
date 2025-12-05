55c65bc814c92606e91343a8a469c2f4f4c9ab19
Keys87-Laptop2 <skykwoo2505@gmail.com>
2025-12-05 12:28:00 +0700

    great dynamic additions update, NOT DONE

---
+++ public/subjects.js
@@ -0,0 +1,43 @@
+export const subjects = {
+  "SAT": [
+    { "subject": "Reading and Writing" },
+    { "subject": "Math" }
+  ],
+  "IELTS": [
+    { "subject": "IELTS" }
+  ],
+  "CIE IGCSE": [
+    { "subject": "Accounting (0452)" },
+    { "subject": "Additional Mathematics (0606)" },
+    { "subject": "Biology (0610)" },
+    { "subject": "Business Studies (0450)" },
+    { "subject": "Chemistry (0620)" },
+    { "subject": "Computer Science (0478)" },
+    { "subject": "Economics (0455)" },
+    { "subject": "English First Language (0500)" },
+    { "subject": "Mathematics (0580)" },
+    { "subject": "Physics (0625)" }
+  ],
+  "IBDP": [
+    { "subject": "English A: Language and Literature" },
+    { "subject": "English B" },
+    { "subject": "Business Management" },
+    { "subject": "Economics" },
+    { "subject": "Environmental Systems and Societies" },
+    { "subject": "Biology" },
+    { "subject": "Chemistry" },
+    { "subject": "Computer Science" },
+    { "subject": "Physics" },
+    { "subject": "Mathematics Analysis and Approaches" },
+    { "subject": "Mathematics Applications and Interpretation" }
+  ],
+  "A-Levels": [
+    { "subject": "Biology (9700)" },
+    { "subject": "Chemistry (9701)" },
+    { "subject": "History (9489)" },
+    { "subject": "Mathematics (9709)" },
+    { "subject": "Further Mathematics (9231)" },
+    { "subject": "Physics (9702)" },
+    { "subject": "Psychology (9990)" }
+  ]
+}
