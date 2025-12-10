6aecbed63ae61fd16cae1d7d91f78f588ef91bca
Keys87-Laptop2 <skykwoo2505@gmail.com>
2025-12-10 09:54:12 +0700

    integrating the scripts to igcse and alevels; final polish

--- public/rss.js
+++ public/rss.js
@@ -80,5 +80,169 @@ export const rss = {
   "Mathematics Applications and Interpretation": [
     ["IB Maths - AI HL Video Lectures", "https://ibmaths.com/ibaihl/"],
     ["Exam Mate - Past Papers", "https://www.exam-mate.com/"]
+  ],
+  "Biology (9700)": [
+    ["A-Level Notes (Weebly) - Biology", "https://alevel-notes.weebly.com/biology.html"],
+    ["PapaCambridge - Biology Past Papers", "https://pastpapers.papacambridge.com/papers/caie/as-and-a-level-biology-9700"],
+    ["ZNotes - Biology Summarised Notes", "https://znotes.org/caie/as-level/biology-9700"],
+    ["Physics and Maths Tutor - Biology Past Papers", "https://www.physicsandmathstutor.com/biology-revision/a-level-cie/"],
+    ["Amoeba Sisters - YouTube Channel", "https://youtube.com/@amoebasisters?si=0x7Ibe7l-wY4O9sI"],
+    ["Google Drive - Edexcel Biology Notes", "https://drive.google.com/drive/folders/1DFY435SOCrR7WHWPc9TP4jpEaP-IqDHn"],
+    ["Google Drive - CAIE Biology Notes", "https://drive.google.com/drive/folders/1bz7EHibAsax7AL1HMgbBf5QrnN6lkXIX"]
+  ],
+
+  "Chemistry (9701)": [
+    ["PapaCambridge - Chemistry Past Papers", "https://pastpapers.papacambridge.com/papers/caie/as-and-a-level-chemistry-9701"],
+    ["ZNotes - Chemistry Summarised Notes", "https://znotes.org/caie/as-level/chemistry-9701"],
+    ["Physics and Maths Tutor - Chemistry Past Papers", "https://www.physicsandmathstutor.com/chemistry-revision/a-level-caie/"],
+    ["Rocket Revise - Chemistry Notes", "https://rocketrevise.com/a-level-cambridge/cie-a-level-biology-9700/cie-9701-a-level-chemistry-as-notes-2025/"],
+    ["Google Drive - Organic Chemistry Notes", "https://drive.google.com/file/d/1lXR3SA9Xkq31895LS-aRDJH2GiGT9jSq/view"],
+    ["Google Drive - Chemistry U1 Topical Notes", "https://drive.google.com/file/d/1TmFdPRQ1-jp9mHKYCTZhxS22oaTcl1lj/view"],
+    ["Google Drive - Chemistry U2 Topical Notes", "https://drive.google.com/file/d/1LpIPcSBDbXn0vHLKLGVH-nXelhFXGEBm/view"],
+    ["Google Drive - Edexcel Chemistry Notes", "https://drive.google.com/drive/folders/1xROczLnmnUHKg_k_rXi3WZXifseiAV8W"],
+    ["Google Drive - CAIE Chemistry Notes", "https://drive.google.com/drive/folders/1ObvMvguHNvyblXzKZEQNfHzoVOX0Orqd"],
+    ["ChemGuide - Comprehensive Chemistry Notes", "https://www.chemguide.co.uk/"]
+  ],
+
+  "History (9489)": [
+    ["YouTube Playlist - History Past Paper Walkthrough", "https://www.youtube.com/playlist?list=PLXtShDmDvfwz76PjyNPKvy8XzO33VvDfx"],
+    ["YouTube Video - History Exam Tips", "https://www.youtube.com/watch?v=CHzT1rvouAc"],
+    ["Google Drive - History Notes and Questions", "https://drive.google.com/drive/folders/1sLI8v37Qb3gVwzZFYQ10OU4W9usZZvi3"],
+    ["CIE A Level Notes - History", "https://www.ciealevelnotes.com/history"],
+    ["Too Lazy to Study - History Notes", "https://www.toolazytostudy.com/history-notes"],
+    ["Study Rocket - History Summaries", "https://studyrocket.co.uk/revision/a-level-history-caie"]
+  ],
+
+  "Mathematics (9709)": [
+    ["Google Drive - Pure Mathematics Topical Notes", "https://drive.google.com/file/d/1Rxvb5wa7SuUWZwkJtCBFNMXmUv1pAk78/view?usp=sharing"],
+    ["Google Drive - Statistics Mathematics Topical Notes", "https://drive.google.com/file/d/1LDIuhiAlbnfh8IvaHZRzM3HGaNkfSvXF/view?usp=sharing"],
+    ["YouTube Playlist - Mathematics Topics", "https://www.youtube.com/playlist?list=PL1UnQ8WsLG10zNSLb3V2J5_HEniUgcl3S"],
+    ["Google Drive - Mathematics Formula Sheet", "https://drive.google.com/file/d/1d3Ytmdyi1RpHNjDeMjrng2a42EGJW0_P/view?usp=sharing"],
+    ["GCE A Level Maths 9709 - YouTube Channel", "https://www.youtube.com/@GCEALevelMaths9709"]
+  ],
+
+  "Further Mathematics (9231)": [
+    ["YouTube Playlist - Further Mathematics Memory Aid", "https://www.youtube.com/playlist?list=PL0SSkmc4r_BZVnf9xaNsScdHUpiu6JfNc"],
+    ["YouTube Playlist - Further Maths Topic Explanations", "https://www.youtube.com/playlist?list=PLg2tfDG3Ww4us-ACGUOMdCiYXyg4ymxrK"],
+    ["YouTube Playlist - Further Maths Past Papers", "https://www.youtube.com/playlist?list=PL5cY5CwQvGSQZJ7AI_Lu-z4K-in-jKZ"],
+    ["A Level Maths Revision - Further Maths Notes", "https://alevelmathsrevision.com/further-maths-revision-notes/"],
+    ["AMSP - Further Mathematics Resources", "https://amsp.org.uk/students/studying-a-level-further-mathematics/resources/"],
+    ["MadAsMaths - Topical Questions", "https://www.madasmaths.com/"],
+    ["Isaac Science - Mathematics Question Decks", "https://isaacscience.org/maths/a_level/question_decks"],
+    ["Underground Mathematics - Question Database", "https://undergroundmathematics.org/"],
+    ["Dr Frost - Mathematics Worksheets", "https://www.drfrost.org/worksheets.php?wdid=2"]
+  ],
+
+  "Physics (9702)": [
+    ["Google Drive - Physics Notes and Definitions", "https://drive.google.com/drive/folders/1HoVQM10VLoTLabCgCpmNTFURqQWtzF9-"],
+    ["Google Drive - Physics Formula Compilation", "https://drive.google.com/file/d/1wX7MDH7wfSdMrnxGKRwYCoKJHsXdJMdP/view"],
+    ["MegaLecture - Physics Notes and Worksheets", "https://megalecture.com/as-level-physics-notes-and-worksheets/"],
+    ["Crack A Level - Physics Resources", "https://crackalevel.wordpress.com/physics/"],
+    ["YouTube Playlist - Physics Topics", "https://www.youtube.com/playlist?list=PLYJ6oM_78KIOHuFk_a5TdX8CXOX8-RAFu"],
+    ["Physics with Moali - YouTube Channel", "https://youtube.com/@physicswithmoali?si=RzLl8j4q5AlrTFvB"],
+    ["Isaac Physics - Topic Index", "https://isaacscience.org/pages/a_level_topic_index"],
+    ["A Level Physics Resources - Google Sites", "https://sites.google.com/view/alevelphysicsresources/home"]
+  ],
+
+  "Psychology (9990)": [
+    ["TES - Psychology Introduction", "https://www.tes.com/teaching-resource/introduction-to-caie-psychology-9990-12375641"],
+    ["Crack A Level - Psychology Resources", "https://crackalevel.wordpress.com/psychology/"],
+    ["Educate A Change - Psychology Course", "https://educateachange.com/courses/psychology-as-a-level-free-course/"],
+    ["Study With Mehar - Psychology Notes", "https://www.studywithmehar.com"],
+    ["9990 Psychology Tutor - YouTube Channel", "https://www.youtube.com/@9990psychologytutor"],
+    ["Psychology Hub - Exam Tips and Notes", "https://psychologyhub.co.uk/"]
+  ],
+  "Accounting (0452)": [
+    ["IGCSE Resources - Accounting Notes", "https://sites.google.com/view/igcseresources/professional-and-creative/accounting"],
+    ["YouTube Playlist - Accounting Topics", "https://www.youtube.com/playlist?list=PLKQK0E6-_cuhLRmkMuJAikjwfNthxiIyV"],
+    ["IGCSE Accounts - Complete Resource Hub", "https://www.igcseaccounts.com/"],
+    ["YouTube Playlist - Accounting Concepts and Terms", "https://www.youtube.com/watch?v=bNC0c60xw7U&list=PL0KARoN8pE2MKCrzzA9Jv1a1gUgRZtR-j&index=1"],
+    ["YouTube Playlist - Accounting Past Paper Walkthroughs", "https://www.youtube.com/watch?v=2hNMEuB-HK8&list=PL5U202Q1noklCDL9aanug54zFofnOlahW"]
+  ],
+  "Additional Mathematics (0606)": [
+    ["YouTube Playlist - Additional Maths Full Syllabus", "IGCSE/O Level Add Maths - Full Syllabus Run Through"],
+    ["Ginger Mathematician - YouTube Channel", "https://www.youtube.com/@GingerMathematician"],
+    ["IGCSE Resources - Additional Maths Solutions", "https://sites.google.com/view/igcseresources/mathematics/additional-mathematics"],
+    ["Revise Right Now - Additional Maths Practice", "https://reviserightnow.com/maths/igcse/additional-maths/straight-line-graphs/exam-questions/cie"]
+  ],
+  "Biology (0610)": [
+    ["IITian Academy - Biology Resources", "https://www.iitianacademy.com/igcse/igcse-biology/"],
+    ["ZNotes - Biology Complete Package", "https://znotes.org/caie/igcse/biology-0610"],
+    ["IGCSE Study Buddy - Biology Video Lectures", "https://youtube.com/@igcsestudybuddy?si=ofqtQ-0aj3TrPbFF"],
+    ["Amoeba Sisters - Biology Fun Learning", "https://youtube.com/@amoebasisters?si=0x7Ibe7l-wY4O9sI"],
+    ["Exam-Mate - Biology Papers Database", "https://www.exam-mate.com/"]
+  ],
+  "Business Studies (0450)": [
+    ["IGCSE Resources - Business Studies Guide", "https://sites.google.com/view/igcseresources/professional-and-creative/business-studies"],
+    ["IGCSE Aid - Business Studies Notes", "https://igcseaid.com/notes/business-studies-0450/"],
+    ["YouTube Playlist - Business Studies Exam Tips", "https://www.youtube.com/watch?v=9Vk2FXWEQVc&list=PLUk3yfbOuRvEm_XCEEDVPBzkq9bR6ouSv&index=1"],
+    ["Cambridge Essentials - Business Studies Channel", "https://www.youtube.com/@CambridgeEssentials"]
+  ],
+  "Chemistry (0620)": [
+    ["Exam-Mate - Chemistry Papers Database", "https://www.exam-mate.com/"],
+    ["ZNotes - Chemistry Complete Package", "https://znotes.org/caie/igcse/chemistry-0620/"],
+    ["IITian Academy - Chemistry Resources", "https://www.iitianacademy.com/igcse/cie-igcse-chemistry/"],
+    ["IGCSE Study Buddy - Chemistry Video Lectures", "https://youtube.com/@igcsestudybuddy?si=ofqtQ-0aj3TrPbFF"],
+    ["IGCSE Online - Chemistry Syllabus Summary", "https://youtube.com/@igcseonline?si=AWgmwoe0DALHlfTQ"]
+  ],
+  "Computer Science (0478)": [
+    ["IGCSE Hub - Computer Science Notes", "https://igcsehub.github.io/cs/index.html"],
+    ["Quizlet - Computer Science Flashcards", "https://quizlet.com/user/Fadeeee/folders/igcse-computer-science/sets"],
+    ["Mr Bulmer's Learning Zone - YouTube Channel", "https://www.youtube.com/@mrbulmerslearningzone/videos"],
+    ["Google Drive - Computer Science Past Papers 2015-Present", "https://drive.google.com/drive/folders/1-CQZbc8dAxai2Qw-bpddpDudpSIASiaT"],
+    ["Google Drive - Computer Science Candidate Responses", "https://drive.google.com/file/d/173OiSMziYexzkTbArliYwvDiW8tUFl2h/view?usp=sharing"],
+    ["Google Drive - 15-Mark Pseudocode Questions", "https://drive.google.com/file/d/1MN_gTKhziJc_Ze3kIgesRESHm4zrtYYX/view?usp=sharing"],
+    ["YouTube Playlist - Computer Science Comprehensive Coverage", "https://www.youtube.com/playlist?list=PLYJ6oM_78KIP7NYKnP_KJ6lDPMhmpYVgA"],
+    ["Google Drive - Programming Syntax Guide", "https://drive.google.com/file/d/1DLDyOa2B7o7V8dZxKULnzP-8Tb54ZMnk/view?usp=sharing"],
+    ["Google Drive - Paper 1 Diagrams", "https://drive.google.com/file/d/1RUwoUERHsc6mYqx759GzkeUdTUDHWN3B/view?usp=sharing"],
+    ["Google Drive - Computer Science Topical Notes", "https://drive.google.com/file/d/1bJ6FBZbWVjmnVMoVLk3YzpNvxQu5_yFb/view?usp=sharing"]
+  ],
+  "Economics (0455)": [
+    ["YouTube Playlist - Economics Detailed Explanations", "https://www.youtube.com/playlist?list=PLL824lXFgrJgkqSIb_zHqMHrXRB0b4ydq"],
+    ["YouTube Video - Economics Concise Review", "https://www.youtube.com/watch?v=uAF6e7XoGs0"],
+    ["YouTube Video - Economics Essential Diagrams", "https://www.youtube.com/watch?v=dU3ftcnuM04"],
+    ["Save My Exams - Economics Notes and Tips", "https://www.savemyexams.com/igcse/economics/"],
+    ["Tutors Plus - Economics Overview", "https://tutorsplus.com/best-revision-resources-for-igcse-economics/"],
+    ["Jadeed Institute - YouTube Channel", "https://www.youtube.com/@JadeedInstitute"]
+  ],
+  "English First Language (0500)": [
+    ["IGCSE1 English - Question and Sample Answers", "https://igcse1english.wordpress.com/"],
+    ["IGCSE SUCCESS - YouTube Channel", "https://www.youtube.com/@IGCSESUCCESS/videos"],
+    ["Taughtly - YouTube Channel", "https://www.youtube.com/@Taughtly/videos"],
+    ["Google Drive - English Explanations and Samples", "https://drive.google.com/drive/folders/17E616E4JqRqCZW4Oc_3bLoFvO-bL-A11"],
+    ["Google Drive - English Text Structure Guide", "https://drive.google.com/drive/folders/1H8Y_h9KEQPxxhP1BUXV3WHLT-bsZOKwK"],
+    ["Google Drive - English Paper Walkthroughs", "https://drive.google.com/drive/folders/1pyuKNJ8qCVN3AKOfFHUexS1pfmtNU1TB?usp=sharing"]
+  ],
+  "Mathematics (0580)": [
+    ["Ginger Mathematician - Mathematics Video Channel", "https://www.youtube.com/@GingerMathematician/videos"],
+    ["Revise Right Now - Mathematics Video Explanations", "https://reviserightnow.com/maths/igcse/extended/cambridge-cie"],
+    ["Google Drive - Mathematics Comprehensive Notes", "https://drive.google.com/drive/folders/1eGyzQxtePO2fzUIeu0nVYitiuKLg_z5N"],
+    ["Google Drive - Mathematics Rules and Formulas", "https://drive.google.com/file/d/1REapALdmPDdkF7CqWtRSe3j1hFv8nJV_/view"],
+    ["DMath Academy - YouTube Channel", "https://www.youtube.com/@DMathAcademy/videos"],
+    ["Google Drive - Mathematics Topical Worksheets", "https://drive.google.com/drive/folders/15_5-MBVOu2qFNoHWeVUKgg8HkZQ5rlZn"]
+  ],
+  "Physics (0625)": [
+    ["Exam-Mate - Physics Papers Database", "https://www.exam-mate.com/"],
+    ["ZNotes - Physics Complete Package", "https://znotes.org/caie/igcse/physics-0625/"],
+    ["IITian Academy - Physics Revision Resources", "https://www.iitianacademy.com/igcse/igcse-cie-physics-revision-resources/"],
+    ["IGCSE Study Buddy - Physics Video Lectures", "https://youtube.com/@igcsestudybuddy?si=ofqtQ-0aj3TrPbFF"],
+    ["Physics with Moali - YouTube Channel", "https://youtube.com/@physicswithmoali?si=RzLl8j4q5AlrTFvB"],
+    ["IGCSE Online - Physics Syllabus Summary", "https://youtube.com/@igcseonline?si=AWgmwoe0DALHlfTQ"]
+  ],
+  "Reading and Writing": [
+    ["Crimson Education - SAT Reading Practice Test", "https://www.crimsoneducation.org/ap/practice-tests/sat/reading"],
+    ["IITian Academy - Digital SAT Preparation", "https://www.iitianacademy.com/digital-sat/?gad_source=1"],
+    ["College Board - Student Question Bank", "https://satsuite.collegeboard.org/practice/student-question-bank"],
+    ["Khan Academy - SAT Reading and Writing Prep", "https://www.khanacademy.org/test-prep/sat-reading-and-writing"],
+    ["College Board Bluebook - Digital SAT Practice", "https://bluebook.collegeboard.org/students"],
+    ["PrepScholar SAT Blog", "https://blog.prepscholar.com/topic/sat"]
+  ],
+  "Math": [
+    ["Crimson Education - SAT Math Practice Test", "https://www.crimsoneducation.org/ap/practice-tests/sat/maths"],
+    ["IITian Academy - Digital SAT Preparation", "https://www.iitianacademy.com/digital-sat/?gad_source=1"],
+    ["College Board - Student Question Bank", "https://satsuite.collegeboard.org/practice/student-question-bank"],
+    ["Khan Academy - SAT Math Prep", "https://www.khanacademy.org/test-prep/v2-sat-math"],
+    ["College Board Bluebook - Digital SAT Practice", "https://bluebook.collegeboard.org/students"],
+    ["PrepScholar SAT Blog", "https://blog.prepscholar.com/topic/sat"]
   ]
-}
+};
--- scripts/rss-desc.js
+++ scripts/rss-desc.js
@@ -1,17 +1,36 @@
 import { desc } from "../public/desc.js";
+import { rss } from "../public/rss.js";
 
-function descApplicator(subject, resource) {
-    const content = desc[subject][resource]
+function descApplicator(resource) {
+    console.log(`descApplicator---current resource: ${resource}`)
+    const content = desc[resource]
     return `<p class="p-8">${content}</p>`
 }
 
-export function renderDesc() {
-    let subject = window.appState.currentSubject
-    let resource = window.appState.currentResource
-    console.log(`current resource: ${resource}`)
-    let element = descApplicator(subject, resource)
-    const parent = document.getElementById("desc")
-    
-    parent.innerHTML = element
+function linkApplicator(resource, subject) {
+    console.log(`linkApplicator---current resource: ${resource}---current subject: ${subject}`)
+    const url = rss[subject][1][1]
+    return `<a href="${url}" class="w-24 sm:h-12 sm:w-32 md:h-16 md:w-40 lg:h-20 lg:w-48 xl:h-24 xl:w-56 2xl:h-28 2xl:w-64 p-2 inline-flex justify-center items-center text-center rounded-2xl shadow-lg transition transform bg-linear-to-r from-[#dde6ef] to-[#cfd9e2] text-black hover:shadow-2xl hover:scale-110 hover:from-[#f5d8cc] hover:to-[#e3c7bc]">link to resource</a>`
+}
 
+export function renderDesc() {
+    let sub = window.appState.currentSubject 
+    let curRss = window.appState.currentResource
+    console.log(`current subject: ${sub}\ncurrent resource: ${window.appState.currentResource}`)
+    let element = descApplicator(curRss)
+    const parent = document.getElementById("desc")
+
+    parent.innerHTML = element
+}
+
+export function renderLinks() {
+    let sub = window.appState.currentSubject 
+    let rss = window.appState.currentResource
+
+    console.log(`current subject: ${sub}\ncurrent resource: ${window.appState.currentResource}`)
+    let element = linkApplicator(rss, sub)
+    const parent = document.getElementById("link")
+
+    parent.innerHTML = element
+    
 }
